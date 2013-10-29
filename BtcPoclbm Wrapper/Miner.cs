5a1a9b17dd94c81b0aaa9daf93baad1985ba0ae9
Nucs <elibelash@gmail.com>
2013-10-29 02:43:13 +0200

    Added event MhashUpdated and overall makeover

--- BtcPoclbm Wrapper/Miner.cs
+++ BtcPoclbm Wrapper/Miner.cs
@@ -1,20 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ProcessReadWriteUtils;
 
-namespace Bitcoin_Stealth_Miner {
+namespace BtcPoclbmWrapper {
+
+    public delegate void MhashUpdatedHandler(double mhps);
     public static class Miner {
         //sample for query: poclbm.exe --device=0 --platform=0 --verbose -r1 elibelash.elibelash:qweqwe@api2.bitcoin.cz:8332 
         
-        #region Properties
+        #region Properties and Events
+
+        public static event MhashUpdatedHandler MhashUpdated;
 
         private static string _minerAppTarget = "poclbm.exe";
         private static string _minerLocation = "\\";
@@ -59,36 +64,57 @@ namespace Bitcoin_Stealth_Miner {
         /// Wether the process of the miner is open. for working status use <see cref="IsMining"/>.
         /// </summary>
         public static bool IsOpen {
-            get { return _miner != null; }
+            get { return _miner != null && _miner.HasExited == false; } //todo perform test if the process actually exists.
         }
 
         /// <summary>
         /// Is currently mining
         /// </summary>
         public static bool IsMining {
-            get { return IsOpen && MhashPerSecond > 0; }
+            get { return IsOpen && (CollectFeedback != false && MhashPerSecond > 0); }
         }
 
         /// <summary>
-        /// Mega hash per second that is being processed by the miner, if inactive - it is 0. (use IsMining prop for this purpose)
+        /// The IO Manager for the program, used to send commands and read/bind the/to output of it, including the MhashPerSecond updates
+        /// </summary>
+        public static ProcessIOManager IOManager {
+            get { return io_proc; }
+        }
+
+        /// <summary>
+        /// Mega hash per second that is being processed by the miner, if inactive - it is 0 or below. (use IsMining prop for this purpose)
         /// </summary>
         public static double MhashPerSecond {
             get { return _megaHashPerSecond; }
             private set { if (!_props_locked) _megaHashPerSecond = value; }
         }
-        
-        private static Process _miner = null; //the miner proc
 
+        /// <summary>
+        /// Should <see cref="MhashPerSecond"/> be collected from the poclbm process?
+        /// True by default.
+        /// </summary>
+        public static bool CollectFeedback {
+            get { return _collectFeedback; }
+            set {
+                if (IsOpen) throw new InvalidOperationException("Cannot change this property after miner is started. Please close it first.");
+                _collectFeedback = value;
+                if (value == false)
+                    _megaHashPerSecond = -1;
+            }
+        }
+        
         /// <summary>
         /// What is the system architecture, 64 or 32 bit. (as int)
         /// </summary>
         public static readonly int SystemBits;
 
+        private static Process _miner = null; //the miner proc
         private static double _megaHashPerSecond;
         private static bool _props_locked = false; //prevent _megaHashPerSecond writing till started again.
-        private static bool _cancelled = false; //thread exiter incase of stop.
         private static StreamReader _redirected_reader = null;
         private static StreamWriter _redirected_writer = null;
+        private static bool _collectFeedback = true;
+        private static ProcessIOManager io_proc = null;
 
         static Miner() {
             SystemBits = string.IsNullOrEmpty(Environment.GetEnvironmentVariable("PROCESSOR_ARCHITEW6432")) ? 32 : 64;
@@ -99,46 +125,71 @@ namespace Bitcoin_Stealth_Miner {
         #region Methods
         #region Public
 
+        /// <summary>
+        /// Starts a controlled poclbm process.
+        /// </summary>
+        /// <param name="url">The mining pool url. e.g. slush's pool: `stratum.bitcoin.cz`</param>
+        /// <param name="port">Mining pool's port. e.g. slush's pool: 3333</param>
+        /// <param name="username">Your worker's username. e.g. elibelash.worker1</param>
+        /// <param name="password">Your worker's password.</param>
+        /// <param name="hide">Should it launch hidden (in the background) or with a window</param>
+        /// <param name="arguments">Extra arguments that you want to pass. Please do not pass the following: `--varbose`;</param>
         public static void Start(string url, ushort port, string username, string password, bool hide = true ,string arguments = "-r1") {
-            if (IsOpen || _cancelled)
-                throw new InvalidOperationException("Unable to start because " + (_cancelled ? "a process that was requested for cancel did not stop yet" : "there is a miner already open"));
+            if (IsOpen)
+                throw new InvalidOperationException("Unable to start because there is a miner already open");
 
             _props_locked = false;
             url = url.Replace("http://", "");
             var args = string.Format("http://{0}:{1}@{2}:{3}", username, password, url, port);
-            var devices = GatherDevices();
-            foreach (var d in devices) 
-                Console.WriteLine("[{0}]{1}", d.Key, d.Value);
 
             _miner = new Process();
-            var StartInfo = new ProcessStartInfo {FileName = "cmd.exe",
-                                                    WindowStyle = ProcessWindowStyle.Hidden,
-                                                    CreateNoWindow = true,
-                                                    RedirectStandardInput = true, 
-                                                    RedirectStandardOutput = true, 
-                                                    UseShellExecute = false};
+            _miner.Exited += (sender, eventArgs) => Stop(); //self disposer
+            
+            var StartInfo = new ProcessStartInfo {  FileName = "cmd.exe",
+                                                    WindowStyle = hide ? ProcessWindowStyle.Hidden : ProcessWindowStyle.Normal,
+                                                    CreateNoWindow = hide,
+                                                    UseShellExecute = false,
+                                                    RedirectStandardInput = true,
+                                                    RedirectStandardOutput = true  };
             _miner.StartInfo = StartInfo;
             _miner.Start();
-
+            io_proc = new ProcessIOManager(_miner);
+            io_proc.StartProcessOutputRead();
+            io_proc.StdoutTextRead += reader;
+            /*@echo off cd " + AppDomain.CurrentDomain.BaseDirectory + MinerLocation 
+                                                    + string.Format("{0} {1} {2}", MinerAppTarget, arguments, args) */
             _redirected_reader = _miner.StandardOutput;
             _redirected_writer = _miner.StandardInput;
-            Task.Run(() => reader());
+
             _redirected_writer.WriteLine("@echo off");
             _redirected_writer.WriteLine("cd " + AppDomain.CurrentDomain.BaseDirectory + MinerLocation); //the command you wish to run.....
             _redirected_writer.WriteLine("{0} {1} {2}", MinerAppTarget, arguments, args);
         }
 
         public static void Stop() {
-            _miner.Close();
-            _redirected_reader.Dispose();
-            _redirected_reader = null;
-            _redirected_writer.Dispose();
-            _redirected_writer = null;
-            _miner.Dispose();
-            _miner = null;
-            _megaHashPerSecond = -1;
+            if (_miner != null) { //todo rethink this part
+                try {
+                    if (_miner.HasExited == false)
+                        _miner.Kill();
+                } catch (Exception) {
+                    if (_miner != null)
+                        _miner.Close();
+                }
+                _miner = null;
+            }
+
+            if (_redirected_reader != null) {
+                _redirected_reader.Dispose();
+                _redirected_reader = null;
+            }
+            if (_redirected_writer != null) {
+                _redirected_writer.Dispose();
+                _redirected_writer = null;
+            }
+            io_proc = null;
+            //all set to null because non of them have a property for Disposed
+            _megaHashPerSecond = -1; //represents that the miner is not mining. wont be updated till the first feedback from miner
             _props_locked = true; //prevents _megaHashPerSecond writing till started again.
-            _cancelled = true; //reader thread exit
         }
 
         /// <summary>
@@ -165,29 +216,15 @@ namespace Bitcoin_Stealth_Miner {
         #endregion
 
         #region Private
-        private static void reader() { //reads the output of the poclbm
-            var r = _redirected_reader;
-            try {
-                while (true) {
-                    while (r.EndOfStream == false) {
-                        var l = r.ReadLine();
-                        if (string.IsNullOrEmpty(l) || l.Contains("    ")) continue; //filtering unneeded messages
-                        var m = _regex_hash.Match(l);
-                        if (m.Success)
-                            MhashPerSecond = /* "Miner - "+ */double.Parse(m.Groups["hps"].Value)/*+" Mhash/s"*/;
-                        Console.WriteLine(l.Replace("\t", ""));
+        private static void reader(string l) { //reads the output of the poclbm
+            if (string.IsNullOrEmpty(l) || l.Contains("    ") || l.Equals("\r\n")) //filtering unneeded messages
+                return;
 
-                    }
-                    if (r.Peek() == -1)
-                        Thread.Sleep(500);
-                    if (_cancelled) { //exiter
-                        _cancelled = false;
-                        return;
-                    }
-                }
-            } catch (ObjectDisposedException) {
-                //thrown when stop has been called
-                _cancelled = false;
+            var m = _regex_hash.Match(l);
+            if (m.Success) {
+                MhashPerSecond = double.Parse(m.Groups["hps"].Value);
+                if (MhashUpdated != null)
+                    MhashUpdated(MhashPerSecond);
             }
         }
         #endregion
